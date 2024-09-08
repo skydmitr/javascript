986e4efd66b32c6477d157f65b491cc8dcfc04d3
Дмитрий <mityaipudovkin@yandex.ru>
2024-09-08 20:05:15 +0300

    Добавлен блок 2.15

---
+++ src/block2_15/block2_15.2.js
@@ -0,0 +1,12 @@
+//Функция min(a, b)
+
+function Min(a,b){
+    return a < b ? a : b;
+}
+
+a = prompt('Введи 1 число', '')
+b = prompt('Введи 2 число', '')
+
+alert('Минимальное число:' + Min(a,b));
+
+
